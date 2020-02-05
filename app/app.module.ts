c89e5afeb9cdc7a88abbeb7bb3544535c49a6fa1
Andrey Namestnikov <andrewnames85@gmail.com>
2020-02-05 15:05:13 -0500

    commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,20 +8,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {HttpClientModule} from '@angular/common/http';
+import { StatusBarComponent } from './status-bar/status-bar.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    StartupScreenComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      StartupScreenComponent,
+      StatusBarComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MatButtonModule,
+      MatIconModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
