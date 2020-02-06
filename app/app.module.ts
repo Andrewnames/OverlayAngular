91ad8b087aebf41c9b946f4fa03db73247a11800
Andrey Namestnikov <andrewnames85@gmail.com>
2020-02-06 13:29:02 -0500

    commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,8 +7,10 @@ import { StartupScreenComponent } from './StartupScreen/startup-screen/startup-s
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {HttpClientModule} from '@angular/common/http';
 import { StatusBarComponent } from './status-bar/status-bar.component';
+
 @NgModule({
    declarations: [
       AppComponent,
@@ -21,6 +23,7 @@ import { StatusBarComponent } from './status-bar/status-bar.component';
       BrowserAnimationsModule,
       MatButtonModule,
       MatIconModule,
+      MatSlideToggleModule,
       HttpClientModule
    ],
    providers: [],
--- app/status-bar/status-bar.component.ts
+++ app/status-bar/status-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+import { app } from 'electron';
 @Component({
   selector: 'app-status-bar',
   templateUrl: './status-bar.component.html',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatusBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'cog-wheel',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/cogWheel.svg'));
+
+    iconRegistry.addSvgIcon(
+      'wrench',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/wrench.svg'));
+
+    iconRegistry.addSvgIcon(
+        'question',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/icons/question.svg'));
+  }
+
+
 
   ngOnInit() {
+  }
+
+  exit() {
+    window.close();
   }
 
 }
